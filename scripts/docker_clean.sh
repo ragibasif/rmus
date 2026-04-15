2cbf3e46a93f80e5c78a1fb1efdc51cbadbcd592
Ragib Asif <182296466+ragibasif@users.noreply.github.com>
2026-04-15 14:40:21 -0400

    refactor: reset repo for golang port

--- scripts/docker_clean.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Stops everything and removes orphaned containers
-docker compose down --remove-orphans
-
-# (Optional) Remove the images to save disk space
-docker rmi rmus-server rmus-db
